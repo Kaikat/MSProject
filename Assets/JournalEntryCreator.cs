c14ed7562849c025fba8ae722b9ece29f68e17e4
Nataly Moreno <nataly_moreno@umail.ucsb.edu>
2017-04-26 17:03:40 -0700

    Add correct animal profile image to CaughtAnimalUI

---
+++ Assets/JournalEntryCreator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JournalEntryCreator : MonoBehaviour {
+
+	string journalEntryPrefab = "JournalEntry";
+	Object journalEntry;
+	List<Object> journalEntries;
+
+	void Awake()
+	{
+		journalEntries = new List<Object> ();
+		Object journalEntry = AssetManager.LoadPrefab ("", journalEntryPrefab);
+	}
+
+	void Start () 
+	{
+		/*Dictionary<AnimalSpecies, List<Animal>> playerAnimals = Service.Request.Player ().GetAnimals ();
+		foreach (KeyValuePair<AnimalSpecies, List<Animal>> keyPair in playerAnimals)
+		{
+			GameObject entry = (GameObject)GameObject.Instantiate (journalEntry);
+			RawImage image = entry.GetComponent<RawImage> ();
+			image.texture = Resources.Load<Sprite> (System.Enum.Parse(AnimalSpecies, keyPair.Key).ToString());
+		}*/
+
+	}
+}
---
+++ Assets/SetJournalEntry.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SetJournalEntry : MonoBehaviour {
+
+	RawImage animalProfile;
+
+	void Awake ()
+	{
+		animalProfile = GetComponentInChildren<RawImage> ();
+		//EventManager.RegisterEvent(GameEvent.
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		//animalProfile	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
