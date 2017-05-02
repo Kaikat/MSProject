60fd66e9616007c50d1c2421eeaea7d4d06782f1
Andrew Tran <andrew.tran345@gmail.com>
2017-05-02 10:19:04 -0700

    Ignoring *.apk, and updated class name of AnimalsUnderObsListener.cs

--- Assets/Scripts/GUI/ButtonListeners/IDCardUI/AnimalsUnderObsListener.cs
+++ Assets/Scripts/GUI/ButtonListeners/IDCardUI/AnimalsUnderObsListener.cs
@@ -2,12 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimalsUnderObs : MonoBehaviour {	
-	
-    // Update is called once per frame
-	void Update () {
-		
-	}
+public class AnimalsUnderObsListener : MonoBehaviour {	
 
     public void Click()
     {
