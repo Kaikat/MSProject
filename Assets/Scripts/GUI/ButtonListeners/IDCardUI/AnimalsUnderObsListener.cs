417f44684ef514b6593926233e52a35ece89abff
Kevin Soto-Ciriaco <ksciriaco@gmail.com>
2017-05-01 23:19:04 -0700

    AnimalUnderObs button functionality

---
+++ Assets/Scripts/GUI/ButtonListeners/IDCardUI/AnimalsUnderObsListener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalsUnderObs : MonoBehaviour {	
+	
+    // Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Click()
+    {
+        EventManager.TriggerEvent(GameEvent.SwitchScreen, ScreenType.AnimalUnderObs);
+    }
+
+}
