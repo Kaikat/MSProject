ce6d586175d634e3b7617df7fe40b5a1d35b0c89
Andrew Tran <andrew.tran345@gmail.com>
2017-05-31 10:02:52 -0700

    Fixed AnimalUnderObservationLoader

--- Assets/Scripts/GUI/ContentLoaders/AnimalsUnderObservationLoader.cs
+++ Assets/Scripts/GUI/ContentLoaders/AnimalsUnderObservationLoader.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimalsUnderObservationLoader : MonoBehaviour {
+public class AnimalsUnderObservationLoader : MonoBehaviour, IShowHideListener {
 
     public GameObject ContentPanel;
+    public GameObject UIObjectScreen;
     private AnimalSpecies animalSpecies;
+    private List<GameObject> animalPrefabs = new List<GameObject>();
     private const string ANIMAL_UNDER_OBS_PREFAB = "AnimalUnderObs";
 
     void Awake()
     {
         EventManager.RegisterEvent<AnimalSpecies>(GameEvent.ViewingAnimalsUnderObservation, SetAnimalSpecies);
+        UIObjectScreen.GetComponent<TaggedShowHide>().listener = this;
     }
 
     void Destroy()
@@ -23,14 +27,32 @@ public class AnimalsUnderObservationLoader : MonoBehaviour {
         animalSpecies = species;
     }
 
-	public void OnShow()
+    void IShowHideListener.OnShow()
     {
-        List<Animal> animals = Service.Request.Player().GetAnimals()[animalSpecies];
-        foreach (Animal animal in animals)
+        try
         {
-            GameObject newPrefab = Instantiate(Resources.Load(ANIMAL_UNDER_OBS_PREFAB)) as GameObject;
-            newPrefab.transform.parent = ContentPanel.transform;
-            
+            Dictionary<AnimalSpecies, List<Animal>> allAnimals = Service.Request.Player().GetAnimals();
+            List<Animal> animals = Service.Request.Player().GetAnimals()[animalSpecies];
+            foreach (Animal animal in animals)
+            {
+                GameObject newPrefab = Instantiate(Resources.Load(ANIMAL_UNDER_OBS_PREFAB)) as GameObject;
+                newPrefab.transform.parent = ContentPanel.transform;
+                animalPrefabs.Add(newPrefab);
+            }
         }
+        catch (KeyNotFoundException e)
+        {
+            Debug.LogWarning("ANIMALSUNDEROBS: " + animalSpecies.ToString() + " not found: " + e.ToString());
+        }
+        
+    }
+
+    void IShowHideListener.OnHide()
+    {
+        foreach (GameObject animalPrefab in animalPrefabs)
+        {
+            GameObject.Destroy(animalPrefab.gameObject);
+        }
+        animalPrefabs.Clear();
     }
 }
