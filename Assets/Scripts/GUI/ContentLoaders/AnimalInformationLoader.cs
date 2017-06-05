283862e1ebbf993260e8ec7c81b4bc9a7e604f50
Andrew Tran <andrew.tran345@gmail.com>
2017-06-05 08:58:43 -0700

    Added GameState manager and new screen (animal description from animl under obs

---
+++ Assets/Scripts/GUI/ContentLoaders/AnimalInformationLoader.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalInformationLoader : MonoBehaviour
+{
+    public RawImage AnimalImage;
+    public Text AnimalNameTitle;
+    public Text AnimalDescription;
+
+    public Text HealthFactor1;
+    public Text HealthFactor2;
+    public Text HealthFactor3;
+
+    void Awake()
+    {
+    }
+
+    /// <summary>
+    /// Fills in fields for animal information, relying on globally set field.
+    /// </summary>
+    /// <param name="animal"></param>
+    void SetTextFields(Animal animal)
+    {
+        // Get animal species name as string
+        string animalName = Service.Request.AnimalName(animal.Species);
+        // Populate fields
+        AnimalImage.texture = Resources.Load<Texture>(animal.Species.ToString());
+        AnimalNameTitle.text = animalName;
+        AnimalDescription.text = Service.Request.AnimalDescription(animal.Species);
+        // Populate health factors
+        HealthFactor1.text = animal.Stats.Health1.ToString();
+        HealthFactor2.text = animal.Stats.Health2.ToString();
+        HealthFactor3.text = animal.Stats.Health3.ToString();
+    }
+
+    void Destroy()
+    {
+    }
+}
---
+++ Assets/Scripts/Managers/GameStateManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+    class GameStateManager : MonoBehaviour
+    {
+        public static Animal CurrentAnimal;
+        public static AnimalSpecies CurrentAnimalSpecies;
+    }
+}
