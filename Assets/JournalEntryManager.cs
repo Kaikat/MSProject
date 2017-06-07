bc2ff9316f0d2bd430dcefb0c377272066642cc9
Andrew Tran <andrew.tran345@gmail.com>
2017-06-06 22:53:27 -0700

    Added onClick for journal rows

--- Assets/JournalEntryManager.cs
+++ Assets/JournalEntryManager.cs
@@ -1,34 +1,57 @@
-﻿using System.Collections;
+﻿using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JournalEntryManager : MonoBehaviour, IShowHideListener
 {
-	public GameObject journalScreen;
+	public GameObject JournalScreen;
 	public List<GameObject> JournalEntries;
+    private Dictionary<int, Animal> journalAnimalObjects;
 
 	void Awake () 
 	{
-		journalScreen.GetComponent<TaggedShowHide> ().listener = this;
+		JournalScreen.GetComponent<TaggedShowHide> ().listener = this;
 	}
 
 	public void OnShow()
 	{
-		List<JournalEntry> journalEntries = Service.Request.PlayerJournal ();
+        List<JournalEntry> journalEntryValues = Service.Request.PlayerJournal();
+
+        // Get Animal objects if in Journal
+        Player player = Service.Request.Player();
+        journalAnimalObjects = player.GetAnimals().Values.SelectMany(x => x)
+                                     .Concat(player.GetReleasedAnimals().Values.SelectMany(x => x))
+                                     .Where(an => journalEntryValues.Any(entry => entry.AnimalID == an.AnimalID))
+                                     .ToDictionary(x => x.AnimalID);
 
 		for (int i = 0; i < JournalEntries.Count; i++)
 		{
-			if (i < journalEntries.Count)
+			if (i < journalEntryValues.Count)
 			{
-				JournalEntries [i].SetActive (true);
-				JournalEntry entry = journalEntries [i];
+				JournalEntries[i].SetActive(true);
+				JournalEntry entry = journalEntryValues [i];
 
-				JournalEntries [i].GetComponent<SetJournalEntry> ().SetJournalEntryElements (entry);
-				JournalEntries [i].SetActive (true);
+				JournalEntries[i].GetComponent<SetJournalEntry> ().SetJournalEntryElements (entry);
+				JournalEntries[i].SetActive(true);
+
+                // Create local copy of index to pass into closure
+                int _i = i;
+                JournalEntries[i].GetComponent<Button>().onClick.AddListener(() =>
+                    {
+                        Animal animal = journalAnimalObjects[journalEntryValues[_i].AnimalID];
+                        Dictionary<string, object> eventDict = new Dictionary<string, object>()
+                        {
+                            { AnimalInformationController.ANIMAL, animal },
+                            { AnimalInformationController.CALLING_SCREEN, ScreenType.Journal }
+                        };
+                        EventManager.TriggerEvent(GameEvent.ViewingAnimalInformation, eventDict);
+                        EventManager.TriggerEvent(GameEvent.SwitchScreen, ScreenType.Caught);
+                    });
 			}
 			else
 			{
-				JournalEntries [i].SetActive (false);
+				JournalEntries[i].SetActive (false);
 			}
 		}
 	}
