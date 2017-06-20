c356f2c292e5c2c92e26b03776a7de5f50b8992d
Andrew Tran <andrew.tran345@gmail.com>
2017-06-20 13:21:28 -0700

    Added dropdowns to account creation screen

---
+++ Assets/Scripts/GUI/ContentLoaders/NewAccountController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class NewAccountController : MonoBehaviour, IShowHideListener
+{
+    public GameObject NewAccountScreen;
+    public Dropdown DayDropdown;
+    public Dropdown MonthDropdown;
+    public Dropdown YearDropdown;
+
+    private void Awake()
+    {
+        NewAccountScreen.GetComponent<TaggedShowHide>().listener = this;
+    }
+
+    public void OnShow()
+    {
+        DayDropdown.ClearOptions();
+        MonthDropdown.ClearOptions();
+        YearDropdown.ClearOptions();
+
+        DayDropdown.AddOptions(Enumerable.Range(1, 31)
+                                         .Select(x => new Dropdown.OptionData(x.ToString()))
+                                         .ToList());
+        MonthDropdown.AddOptions(Enumerable.Range(1, 12)
+                                           .Select(x => new Dropdown.OptionData(x.ToString()))
+                                           .ToList());
+        YearDropdown.AddOptions(Enumerable.Range(1900, 117)
+                                           .Reverse()
+                                           .Select(x => new Dropdown.OptionData(x.ToString()))
+                                           .ToList());
+    }
+
+    public void OnHide()
+    {
+
+    }
+}
