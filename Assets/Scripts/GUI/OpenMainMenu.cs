bab11ef4e75298cfe4e7d865254adbd478b525fb
Nataly Moreno <nataly_moreno@umail.ucsb.edu>
2017-11-06 23:01:36 -0800

    Add icon to home for the main menu

--- Assets/Scripts/GUI/OpenMainMenu.cs
+++ Assets/Scripts/GUI/OpenMainMenu.cs
@@ -6,10 +6,11 @@ using UnityEngine.UI;
 public class OpenMainMenu : MonoBehaviour 
 {
 	public Image ButtonImageAvatar;
+	private readonly string BUTTON = "Button";
 
 	void Start()
 	{
-		ButtonImageAvatar.sprite = Resources.Load<Sprite> (Service.Request.Player ().Avatar.ToString ());
+		ButtonImageAvatar.sprite = Resources.Load<Sprite> (Service.Request.Player ().Avatar.ToString () + BUTTON);
 	}
 
 	public void Click()
