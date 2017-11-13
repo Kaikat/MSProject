a44412d48ce117ff72bd6becaabb10175cded38f
Nataly Moreno <nataly_moreno@umail.ucsb.edu>
2017-11-13 02:19:30 -0800

    Add animal image, spanish, and nahuatl names to descriptions on the animal list screen

--- Assets/Scripts/GUI/ButtonListeners/AnimalsDescUI/SetAnimalDescriptionEntry.cs
+++ Assets/Scripts/GUI/ButtonListeners/AnimalsDescUI/SetAnimalDescriptionEntry.cs
@@ -5,16 +5,25 @@ using UnityEngine.UI;
 
 public class SetAnimalDescriptionEntry : MonoBehaviour 
 {
+	public RawImage AnimalImage;
 	public Text AnimalSpecies;
+	public Text SpanishName;
+	public Text NahuatlName;
 	public Text Description;
 	public Text Venue;
 
 	public Image Background;
 	public Image DataPanel;
 
+	private readonly string SPANISH = "Spanish: ";
+	private readonly string NAHUATL = "Nahuatl: ";
+
 	public void SetAnimalDescription(AnimalData animalData)
 	{
+		AnimalImage.texture = Resources.Load<Texture> (UIConstants.ANIMAL_IMAGE_PATH + animalData.Species.ToString ());
 		AnimalSpecies.text = animalData.Name;
+		SpanishName.text = SPANISH + animalData.SpanishName;
+		NahuatlName.text = NAHUATL + animalData.NahuatlName;
 		Description.text = animalData.Description;
 
 		DataPanel.color = UIConstants.Beige;
