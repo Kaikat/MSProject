72d1890a33da237b446ee42e9235d4c4904ad6bf
Nataly Moreno <nataly_moreno@umail.ucsb.edu>
2017-11-15 23:34:11 -0800

    Add a scene with the splash screen because Android Splash is broken in Unity. Add a loding background that becomes blurry and has no option to sharpen in Unity so that the user is not left with a black screen on Android. Remove old unused scenes

---
+++ Assets/LoadRealScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadRealScene : MonoBehaviour 
+{
+	private readonly string MAIN_SCENE = "ScreenManagerChange";
+	private float timer = 1.0f;
+
+	void Update ()
+	{
+		timer -= Time.deltaTime;
+		if (timer <= 0.0f)
+		{
+			SceneManager.LoadScene(MAIN_SCENE, LoadSceneMode.Single);
+		}
+	}
+}
